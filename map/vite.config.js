bbb3e181587cc9caf65a9e1481163f8abc4e51d0
elymperis <elymperis@getmap.gr>
2023-05-30 15:01:39 +0300

    Change Vite base path

--- map/vite.config.js
+++ map/vite.config.js
@@ -10,7 +10,7 @@ let outDir = resolve(__dirname, 'dist')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:  '/pindus-demo/',
+  base:  '/web-map/',
   build: { 
     outDir,
     emptyOutDir: true,
